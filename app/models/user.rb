9d25c78b925e5963976aa05737cae703c86bf252
Kevin McCormack <wittyman37@yahoo.com>
2016-02-27 15:29:07 -0500

    model tests passing

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :carts
+
+  def current_cart=(cart)
+    carts << cart
+  end
+
+  def current_cart
+    carts.last
+  end
+end
