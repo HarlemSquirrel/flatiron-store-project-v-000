d3e876e5766b58d04859f8f26ceb4e1166df6ea5
Kevin McCormack <wittyman37@yahoo.com>
2016-02-28 15:25:48 -0500

    all tests passing

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,6 +2,8 @@ class StoreController < ApplicationController
   def index
     @categories = Category.all
     @items = Item.available_items
-    @cart = current_user.current_cart
+    if !!current_user
+      @cart = current_user.current_cart
+    end
   end
 end
